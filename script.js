48077ddb23bfd68f600e522bb007da98a2ca442e
Aaryavardhun <aaryavardhun@gmail.com>
2023-10-22 16:43:36 -0500

    Implemented filter functionality

--- script.js
+++ script.js
@@ -51,6 +51,21 @@ function clearItems(){
     while(itemList.firstChild){
         itemList.removeChild(itemList.firstChild);
     }
+    checkUI();
+}
+
+function filterItems(e){
+    const items = itemList.querySelectorAll('li');
+    const text = e.target.value.toLowerCase();
+    items.forEach((item) => {
+        const itemName = item.firstChild.textContent.toLowerCase();
+        if(itemName.indexOf(text) != -1){
+            item.style.display = 'flex';
+        }else{
+            item.style.display = 'none';
+        }
+    });
+    
 }
 
 function checkUI(){
@@ -69,3 +84,4 @@ checkUI();
 itemForm.addEventListener('submit', addItem);
 itemList.addEventListener('click', removeItem);
 clearBtn.addEventListener('click',clearItems);
+itemFilter.addEventListener('input',filterItems);
